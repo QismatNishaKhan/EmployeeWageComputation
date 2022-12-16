d197b087ddfbc57ea0482327898637dc9fbc94ee
qismat <qismatnisha6243@gmail.com>
2022-12-16 09:39:07 +0530

    add UC1-Attedance

---
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeRoll employeeRoll = new EmployeeRoll();
+            employeeRoll.Attendance();
+        }  
+    }
+}
